f1be21a999f893966ee63314f561c56fe6f6cc53
shivakumaarmgs <shivakumaarmgs@gmail.com>
2014-05-10 18:56:23 +0530

    day care id added to events model

--- db/migrate/20131203105320_create_fullcalendar_engine_events.rb
+++ db/migrate/20131203105320_create_fullcalendar_engine_events.rb
@@ -8,9 +8,11 @@ class CreateFullcalendarEngineEvents < ActiveRecord::Migration
       t.date :untildate
       t.string :event_type
       t.string :classroom
+      t.integer :day_care_id
       t.integer :event_series_id
       t.timestamps
     end
     add_index :fullcalendar_engine_events, :event_series_id
+    add_index :fullcalendar_engine_events, :day_care_id
   end
 end
